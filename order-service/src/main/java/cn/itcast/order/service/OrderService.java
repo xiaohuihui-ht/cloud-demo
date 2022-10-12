3cd218c0f1c2b8ba538660d4fd36eeaf324e12b0
xiaohuihui-ht <460759240@qq.com>
2022-10-12 19:23:25 +0800

    使用http客户端Feign，发起远程调用

--- order-service/src/main/java/cn/itcast/order/service/OrderService.java
+++ order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package cn.itcast.order.service;
 
+import cn.itcast.order.clients.UserClient;
 import cn.itcast.order.mapper.OrderMapper;
 import cn.itcast.order.pojo.Order;
 import cn.itcast.order.pojo.User;
@@ -14,15 +15,14 @@ public class OrderService {
     private OrderMapper orderMapper;
 
     @Autowired
-    private RestTemplate restTemplate;
+    private UserClient userClient;
 
     public Order queryOrderById(Long orderId) {
         // 1.查询订单
         Order order = orderMapper.findById(orderId);
 
-        // 2.利用restTemplate发http请求，查询用户
-        String url = "http://userservice/user/" + order.getUserId();
-        User user = restTemplate.getForObject(url, User.class);
+        // 2.用Feign远程调用
+        User user = userClient.findById(order.getUserId());
 
         // 3.封装用户到订单信息
         order.setUser(user);
@@ -30,4 +30,19 @@ public class OrderService {
         // 4.返回
         return order;
     }
+
+//    public Order queryOrderById(Long orderId) {
+//        // 1.查询订单
+//        Order order = orderMapper.findById(orderId);
+//
+//        // 2.利用restTemplate发http请求，查询用户
+//        String url = "http://userservice/user/" + order.getUserId();
+//        User user = restTemplate.getForObject(url, User.class);
+//
+//        // 3.封装用户到订单信息
+//        order.setUser(user);
+//
+//        // 4.返回
+//        return order;
+//    }
 }
