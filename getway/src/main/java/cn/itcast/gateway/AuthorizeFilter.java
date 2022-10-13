791f6ee32dcd24704fa9e63be6ed1c46fc5e4c0c
xiaohuihui-ht <460759240@qq.com>
2022-10-13 17:13:14 +0800

    实现Ordered接口 备注： 实现全局过滤器的步骤？ 1、实现GlobalFilter接口 2、添加@Order注解或实现Ordered接口

--- getway/src/main/java/cn/itcast/gateway/AuthorizeFilter.java
+++ getway/src/main/java/cn/itcast/gateway/AuthorizeFilter.java
@@ -2,6 +2,7 @@ package cn.itcast.gateway;
 
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
@@ -10,9 +11,9 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
-@Order(-1)
+//@Order(-1)
 @Component
-public class AuthorizeFilter implements GlobalFilter {
+public class AuthorizeFilter implements GlobalFilter, Ordered {
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         // 1.获取请求参数
@@ -34,5 +35,10 @@ public class AuthorizeFilter implements GlobalFilter {
 
         // 5.2.拦截请求
         return exchange.getResponse().setComplete();
+    }
+
+    @Override
+    public int getOrder() {
+        return -1;
     }
 }
